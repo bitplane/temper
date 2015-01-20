9586079e832112ca1f8d3aae141c7719c35e14a1
Axel von Bertoldi <bertoldia@gmail.com>
2015-01-19 21:17:15 -0700

    Use new methods
    Now the gist of the main loop is easier to follow.

--- temper.c
+++ temper.c
@@ -53,46 +53,12 @@ void print_temp(float tempc) {
 }
 
 int main(){
-  int passes = 0;
-  float tempc = 0.0000;
-  do {
-    usb_dev_handle* lvr_winusb = pcsensor_open();
+  float tempc = read_temp();
 
-    if (!lvr_winusb) {
-      /* Open fails sometime, sleep and try again */
-      sleep(3);
-    }
-    else {
-
-      tempc = pcsensor_get_temperature(lvr_winusb);
-      pcsensor_close(lvr_winusb);
-    }
-    ++passes;
-  }
-  /* Read fails silently with a 0.0 return, so repeat until not zero
-     or until we have read the same zero value 3 times (just in case
-     temp is really dead on zero */
-  while ((tempc > -0.0001 && tempc < 0.0001) || passes >= 4);
-
-  if (!((tempc > -0.0001 && tempc < 0.0001) || passes >= 4)) {
-    /* Apply calibrations */
-    tempc = (tempc * scale) + offset;
-
-    struct tm *utc;
-    time_t t;
-    t = time(NULL);
-    utc = gmtime(&t);
-
-    char dt[80];
-    strftime(dt, 80, "%d-%b-%Y %H:%M", utc);
-
-    printf("%s,%f\n", dt, tempc);
-    fflush(stdout);
-
+  if (errno == ENOENT) {
+    return 1;
+  } else {
+    print_temp(correct(tempc));
     return 0;
   }
-  else {
-    return 1;
-  }
-
 }
